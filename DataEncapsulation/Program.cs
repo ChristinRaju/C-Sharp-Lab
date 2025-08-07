c3a4adcb4d9a73392c198b082cbf22c90608c80e
ChristinRaju <2006christinraju@gmail.com>
2025-08-07 14:49:45 +0530

    Add files via upload

---
+++ DataEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new Person object
+            Person person = new Person();
+
+            // Get Name and Age
+            Console.Write("Enter name: ");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            person.Age = int.Parse(Console.ReadLine());
+
+            // Display the details
+            Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+        }
+    }
+}
+
