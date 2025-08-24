e5d23798e38e1026fefbb233646583db8714a4a7
ChristinRaju <2006christinraju@gmail.com>
2025-08-24 13:53:20 +0530

    Add files via upload

---
+++ AbstractAnimalExample/AbstractAnimalExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AbstractAnimalExample
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myDog = new Dog();
+            myDog.MakeSound(); // Outputs: Woof!
+            Animal myCat = new Cat();
+            myCat.MakeSound(); // Outputs: Meow!
+        }
+    }
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+}
