9c439d706d51cd40add87ff3f78ab79c4686ffaa
ChristinRaju <2006christinraju@gmail.com>
2025-08-27 21:16:15 +0530

    Add files via upload

---
+++ ExceptionhandlingDemo/ExceptionhandlingDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExceptionhandlingDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+
+                int result = x / y;
+                Console.WriteLine($"Result: {result}");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+                Console.WriteLine($"Exception Message: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Execution completed.");
+            }
+
+        }
+    }
+}
