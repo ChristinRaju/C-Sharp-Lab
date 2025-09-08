48cf1bfd77759e97f549869cc2c3936de0a07148
ChristinRaju <2006christinraju@gmail.com>
2025-09-08 20:29:34 +0530

    Add files via upload

---
+++ SubstringDemo/SubstringDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class SubstringDemo
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string s = Console.ReadLine();
+
+        // Print all substrings
+        for (int start = 0; start < s.Length; start++)
+        {
+            for (int length = 1; length <= s.Length - start; length++)
+            {
+                Console.WriteLine(s.Substring(start, length));
+            }
+        }
+
+        // Print total number of substrings
+        Console.WriteLine("Total substrings: " + (s.Length * (s.Length + 1) / 2));
+    }
+}
