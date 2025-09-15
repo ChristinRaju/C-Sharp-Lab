c4f14165f3f945f8e471714f7140e710e986d666
ChristinRaju <2006christinraju@gmail.com>
2025-09-15 20:44:08 +0530

    Add files via upload

---
+++ NumberReverser/NumberReverser/NumberReverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class NumberReverser
+{
+    private int number;
+    private int reversed;
+
+    public void ReadNumber()
+    {
+        Console.Write("Enter a number: ");
+        number = int.Parse(Console.ReadLine());
+
+        int num = number;
+        reversed = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;      // take last digit
+            reversed = reversed * 10 + digit; // add digit to reverse
+            num = num / 10;            // remove last digit
+        }
+    }
+
+    // Method to display the reversed number
+    public void Display()
+    {
+        Console.WriteLine($"Reverse of {number} is {reversed}");
+    }
+}
+
---
+++ NumberReverser/NumberReverser/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        NumberReverser obj = new NumberReverser();
+        obj.ReadNumber();
+        obj.Display();
+    }
+}
