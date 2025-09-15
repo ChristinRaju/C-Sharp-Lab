977ee18d612151be78726c0a5ddced14795c1ac8
ChristinRaju <2006christinraju@gmail.com>
2025-09-15 20:42:29 +0530

    Add files via upload

---
+++ NumberStatus/NumberStatus/NumberStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class NumberStatus
+{
+    private int number;
+
+    public void ReadNumber()
+    {
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+
+    // Method to check and display result
+    public void DisplayStatus()
+    {
+        if (number > 0)
+            Console.WriteLine($"{number} is Positive.");
+        else if (number < 0)
+            Console.WriteLine($"{number} is Negative.");
+        else
+            Console.WriteLine("The number is Zero.");
+    }
+}
---
+++ NumberStatus/NumberStatus/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of NumberStatus class
+        NumberStatus obj = new NumberStatus();
+
+        obj.ReadNumber();     // Take input
+        obj.DisplayStatus();  // Show result
+    }
+}
