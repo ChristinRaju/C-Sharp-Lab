d6f26037a510aa9e334eae83120ac67d73c83eee
ChristinRaju <2006christinraju@gmail.com>
2025-09-17 22:26:44 +0530

    Add files via upload

---
+++ DisposeExample/DisposeExample/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class FileManager : IDisposable
+{
+    private StreamWriter file;
+
+    public FileManager(string path)
+    {
+        file = new StreamWriter(path);
+        Console.WriteLine("File opened");
+        Console.WriteLine("File created at: " + Path.GetFullPath("test.txt"));
+
+    }
+
+    public void Write(string text)
+    {
+        file.WriteLine(text);
+    }
+
+    public void Dispose()
+    {
+        file.Close();
+        Console.WriteLine("File closed");
+    }
+}
+
---
+++ DisposeExample/DisposeExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        using (FileManager fm = new FileManager("test.txt"))
+        {
+            fm.Write("Hello World!");
+        }
+    }
+}
