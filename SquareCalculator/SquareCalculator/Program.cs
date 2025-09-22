65cc7c5bd07a388d919847f4bda9d07e2d009a4a
ChristinRaju <2006christinraju@gmail.com>
2025-09-22 18:46:00 +0530

    Add files via upload

---
+++ SquareCalculator/SquareCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of SquareCalculator
+        SquareCalculator obj = new SquareCalculator();
+
+        obj.ReadNumber();     // Read input
+        obj.DisplaySquare();  // Print square
+    }
+}
---
+++ SquareCalculator/SquareCalculator/SquareCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class SquareCalculator
+{
+    private int number;
+
+    public void ReadNumber()
+    {
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+
+    // Method to calculate and display square
+    public void DisplaySquare()
+    {
+        int square = number * number;
+        Console.WriteLine($"Square of {number} is {square}");
+    }
+}
