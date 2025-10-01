4070107731cb2ed6246a8a68a0971dfbf1a614cd
ChristinRaju <2006christinraju@gmail.com>
2025-10-01 19:54:08 +0530

    Add files via upload

---
+++ ReadPerson/ReadPerson/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of Person class
+        Person p1 = new Person();
+
+        p1.ReadPerson();     // Read details
+        p1.DisplayPerson();  // Show details
+    }
+}
---
+++ ReadPerson/ReadPerson/ReadPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Person
+{
+    private string name;
+    private int age;
+
+    // Method to read person details
+    public void ReadPerson()
+    {
+        Console.Write("Enter Name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Enter Age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+    }
+
+    // Method to display person details
+    public void DisplayPerson()
+    {
+        Console.WriteLine("\n--- Person Details ---");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age : {age}");
+    }
+}
