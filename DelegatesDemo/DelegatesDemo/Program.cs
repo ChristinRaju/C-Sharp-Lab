e51440e98c886e9cdfbf8879cedb15c3471c6378
ChristinRaju <2006christinraju@gmail.com>
2025-10-02 14:19:55 +0530

    Add files via upload

---
+++ DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    // Step 1: Declare delegate
+    public delegate void PrintMessage(string message);
+
+    // Step 2: Method with same signature
+    static void SayHello(string message)
+    {
+        Console.WriteLine("Hello, " + message);
+    }
+
+    public static void Main()
+    {
+        // Step 3: Create delegate object
+        PrintMessage del = SayHello;
+
+        // Step 4: Call delegate
+        del("Students");
+    }
+}
