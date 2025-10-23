f63356c65565f4db69a4ec859749ba2059835662
ChristinRaju <2006christinraju@gmail.com>
2025-10-23 14:24:31 +0530

    Add files via upload

---
+++ EventHandler.Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EventHandlerDemo
+{
+    // Publisher class
+    public class Publisher
+    {
+        // Define the delegate
+        public delegate void MyEventHandler(string message);
+
+        // Declare the event using the delegate
+        public event MyEventHandler OnChange;
+
+        // Method to trigger the event
+        public void TriggerEvent(string msg)
+        {
+            Console.WriteLine("Publisher: Event triggered");
+            if(OnChange != null )
+                OnChange(msg);
+        }
+    }
+
+    // Subscriber class
+    public class Subscriber
+    {
+        public void HandleEvent(string message)
+        {
+            Console.WriteLine($"Subscriber: Received message - {message}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Publisher and Subscriber
+            Publisher publisher = new Publisher();
+            Subscriber subscriber = new Subscriber();
+
+            publisher.OnChange += subscriber.HandleEvent;
+
+            publisher.TriggerEvent("Hello, C# event.");
+        }
+    }
+}
