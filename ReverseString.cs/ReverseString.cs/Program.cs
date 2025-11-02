7be9678b08272438e066e7d1c1da23c6d3669b39
ChristinRaju <2006christinraju@gmail.com>
2025-11-02 18:36:00 +0530

    Add files via upload

---
+++ ReverseString.cs/ReverseString.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+
+        string reversedWords = string.Join(" ", input
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Reverse());
+
+        Console.WriteLine("Reversed sentence: " + reversedWords);
+    }
+}
